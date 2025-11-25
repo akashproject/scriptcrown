981ffca5a45e72a1bdfb583330ea66466e15f005
akashproject <akashdutta.scriptcrown@gmail.com>
2025-11-25 17:46:14 +0530

    update 2025

--- resources/views/pages/about-us.blade.php
+++ resources/views/pages/about-us.blade.php
@@ -1,13 +1,27 @@
 @extends('layouts.main')
     @section('content')
-    <div class="breadcumb-wrapper " data-bg-src="{{ url('assets/frontend/img/bg/breadcumb-bg.jpg') }}">
+    <div class="breadcumb-wrapper " data-bg-src="{{ url('assets/frontend/img/service/service-inner-banner.webp') }}">
         <div class="container">
-            <div class="breadcumb-content">
-                <h1 class="breadcumb-title">About Us</h1>
-                <ul class="breadcumb-menu">
-                    <li><a href="index.html">Home</a></li>
-                    <li>About Us</li>
-                </ul>
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="breadcumb-content py-3">
+                        <ul class="breadcumb-menu">
+                            <li><a href="index.html">Home</a></li>
+                            <li>{{ $contentMain->name }}</li>
+                        </ul>
+                        <h1 class="breadcumb-title">Custom {{ $contentMain->name }} Solutions That Drive Results</h1>
+                    </div>
+                    <div class="banner-content py-3">
+                        <p>We build high-quality, responsive, and user-friendly websites tailored to your business needs. From modern UI/UX design to powerful backend development, our team delivers fast, secure, and scalable web solutions that help you grow your online presence and convert visitors into customers.</p>
+                        <div>
+                            <span class="mx-2 award-icon"><img src="https://dummyimage.com/100" ></span>
+                            <span class="mx-2 award-icon"><img src="https://dummyimage.com/100" ></span>
+                            <span class="mx-2 award-icon"><img src="https://dummyimage.com/100" ></span>
+                            <span class="mx-2 award-icon"><img src="https://dummyimage.com/100" ></span>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div><!--==============================
